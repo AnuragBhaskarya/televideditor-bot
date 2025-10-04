49b393e8cb7979ac8a429a2e9681b59ff3f15bf1
Anurag Bhaskarya <anuragbhaskarya@gmail.com>
2025-10-04 12:52:50 +0530

    Decrease polling sleep time to improve responsiveness
    Reduce sleep duration from 300 seconds to 3 seconds for polling.

--- televideditor.py
+++ televideditor.py
@@ -394,7 +394,7 @@ if __name__ == '__main__':
             break  # Exit the polling loop
         
         # Wait for 1 second before polling again
-        time.sleep(300)
+        time.sleep(3)
     
     # --- Processing Phase ---
     if first_job:
